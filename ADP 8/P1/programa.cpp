1bb38e9d0286d6aad173bb6edc92fbb470990191
Jose Luis Cruz <j0z3ph@gmail.com>
2026-02-05 15:52:43 -0600

    Primer programa

---
+++ ADP 8/P1/programa.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file programa.cpp
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C++ :D
+ * @version 0.1
+ * @date 2026-02-05
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Holi " << 5.6 << endl;
+    
+    return 0;
+}
