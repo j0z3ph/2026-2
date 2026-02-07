8f3f5e0d9d497f4e0f31d9723d7b53f78636fd4d
Jose Luis Cruz <j0z3ph@gmail.com>
2026-02-06 18:50:01 -0600

    Ejemplos

---
+++ PE/P1/programa.c
@@ -0,0 +1,17 @@
+/**
+ * @file programa.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C :>
+ * @version 0.1
+ * @date 2026-02-06
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    printf("Hola\n");
+    return 0;
+}
