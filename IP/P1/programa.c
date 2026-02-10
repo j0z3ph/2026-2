0cb89029f00d7b6422fcfdecb322b6a4ab468ac8
j0z3ph <j0z3ph@gmail.com>
2026-02-09 20:21:27 -0600

    Ejemplos

---
+++ IP/P1/programa.c
@@ -0,0 +1,19 @@
+/**
+ * @file programa.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer programa en C :>
+ * @version 0.1
+ * @date 2026-02-09
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */ 
+#include<stdio.h>
+
+int main()
+{
+    printf("Holi Canoli\n");
+    
+    return 0;
+}
+
---
+++ IP/P1/programa2.c
@@ -0,0 +1,17 @@
+/**
+ * @file programa2.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Jugando con entradas y salidas estandares :>
+ * @version 0.1
+ * @date 2026-02-09
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    putc('A', stdout);
+    return 0;
+}
