3a6cca77a93e91f5293d9600c686162b14d82b07
Jose Luis Cruz <j0z3ph@gmail.com>
2026-02-13 18:58:36 -0600

    ejemplo

---
+++ PE/P4/sumas.c
@@ -0,0 +1,29 @@
+/**
+ * @file sumas.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Hace sumas 🥳
+ * @version 0.1
+ * @date 2026-02-13
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    float numero1, numero2;
+
+    printf("Usuario, ingresa un numero 🥰 ");
+    scanf("%f", &numero1);
+
+    printf("Ahora, ingresa otro numero 🫀 ");
+    scanf("%f", &numero2);
+
+    printf("% f\n% f\n", numero1, numero2);
+
+    //printf("La suma de %020.1f y %.0f es %f 🖕🏿\n", numero1, numero2, numero1 + numero2);
+
+    
+    return 0;
+}
