025eb23c1599ecad3a4c0c5fef39e3b3257d7837
j0z3ph <j0z3ph@gmail.com>
2026-02-17 20:18:59 -0600

    Ejemplo

---
+++ IP/P5/programa.c
@@ -0,0 +1,24 @@
+/**
+ * @file programa.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Jugando con enteros
+ * @version 0.1
+ * @date 2026-02-17
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    int a = 5;
+
+    printf("%u\n", ~a);
+
+    printf("%d\n", ++a);
+
+    printf("%i\n", a >> 3);
+    
+    return 0;
+}
