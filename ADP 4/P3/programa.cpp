2687598674ed2ecd6bc3c8c4023ac4561ca0a1b2
Jose Luis Cruz <j0z3ph@gmail.com>
2026-02-19 14:37:28 -0600

    ejemplos

---
+++ ADP 4/P3/programa.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file programa.cpp
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer clase 🤖
+ * @version 0.1
+ * @date 2026-02-19
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Persona {
+    private:
+    string nombre;
+    int edad;
+
+    public:
+
+    // Setters
+    void setNombre(string nombre) {
+        this->nombre = nombre;
+    }
+
+
+};
+
+
+int main()
+{
+    Persona p;
+    
+    return 0;
+}
