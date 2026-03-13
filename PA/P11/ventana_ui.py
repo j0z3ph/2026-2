5372d7114ea1756c9655cd0f01b1c282453a6a82
j0z3ph <j0z3ph@gmail.com>
2026-03-13 11:23:58 -0600

    Ventanas

--- PA/P11/ventana_ui.py
+++ PA/P11/ventana_ui.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'ventana.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.9.0
+## Created by: Qt User Interface Compiler version 6.9.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -16,24 +16,28 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenu, QMenuBar,
-    QSizePolicy, QWidget)
+from PySide6.QtWidgets import (QApplication, QHeaderView, QMainWindow, QMenu,
+    QMenuBar, QSizePolicy, QTableWidget, QTableWidgetItem,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(438, 347)
+        MainWindow.resize(402, 316)
         self.actionNuevo = QAction(MainWindow)
         self.actionNuevo.setObjectName(u"actionNuevo")
         self.actionSalir = QAction(MainWindow)
         self.actionSalir.setObjectName(u"actionSalir")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.tabla = QTableWidget(self.centralwidget)
+        self.tabla.setObjectName(u"tabla")
+        self.tabla.setGeometry(QRect(30, 30, 341, 211))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 438, 24))
+        self.menubar.setGeometry(QRect(0, 0, 402, 33))
         self.menuArchivo = QMenu(self.menubar)
         self.menuArchivo.setObjectName(u"menuArchivo")
         MainWindow.setMenuBar(self.menubar)
@@ -49,9 +53,15 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.actionNuevo.setText(QCoreApplication.translate("MainWindow", u"Nuevo", None))
-        self.actionSalir.setText(QCoreApplication.translate("MainWindow", u"Salir", None))
-        self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"Archivo", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Ventana", None))
+        self.actionNuevo.setText(QCoreApplication.translate("MainWindow", u"&Nuevo", None))
+#if QT_CONFIG(shortcut)
+        self.actionNuevo.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+N", None))
+#endif // QT_CONFIG(shortcut)
+        self.actionSalir.setText(QCoreApplication.translate("MainWindow", u"&Salir", None))
+#if QT_CONFIG(shortcut)
+        self.actionSalir.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+S", None))
+#endif // QT_CONFIG(shortcut)
+        self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"&Archivo", None))
     # retranslateUi
 
