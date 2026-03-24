cdb5bf39533dc9c2ef5d233356e0179ad12b845e
j0z3ph <j0z3ph@gmail.com>
2026-03-23 18:55:43 -0600

    Recursion

---
+++ PE/P12/ejemplo.c
@@ -0,0 +1,20 @@
+/**
+ * @file ejemplo.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Ejemplo de recursion
+ * @version 0.1
+ * @date 2026-03-23
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include <stdio.h>
+
+int main()
+{
+    //while(100) {
+        printf("Hola\n");
+    //}
+    main();
+    return 0;
+}
