facb42311ad3f04ac6d730e0757e254d3d7a390b
j0z3ph <j0z3ph@gmail.com>
2026-03-24 20:23:22 -0600

    Recursion

---
+++ IP/P14/recursion.c
@@ -0,0 +1,20 @@
+/**
+ * @file recursion.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief Mi primer recursion
+ * @version 0.1
+ * @date 2026-03-24
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#include <stdio.h>
+
+int main()
+{
+    
+    printf("Hola\n");
+    main();
+    
+    return 0;
+}
