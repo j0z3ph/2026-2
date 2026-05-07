eaf7167c23ffdf4ff0a909cf856345582e8dd069
j0z3ph <j0z3ph@gmail.com>
2026-05-06 20:32:35 -0600

    Dnamica

---
+++ IP/P23/fifo.c
@@ -0,0 +1,100 @@
+/**
+ * @file fifo.c
+ * @author Jose Luis Cruz (jlcruz@ipn.mx)
+ * @brief FIFO
+ * @version 0.1
+ * @date 2026-05-06
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tools.h"
+
+int main()
+{
+    char *menu[] = {"Agregar", "Eliminar", "Listar", "Salir"};
+    int seleccion;
+    int *arreglo = NULL;
+    int tamanio = 0;
+    int valor;
+
+    do
+    {
+        seleccion = showMenu(4, menu, "FIFO");
+
+        if (seleccion == 0)
+        {
+            // Agregar
+            limpiarPantalla();
+            printf("Ingresa un valor: ");
+            scanf("%i", &valor);
+            limpiaBuffer();
+
+            if (arreglo == NULL)
+            {
+                // Lista vacia
+                arreglo = malloc(sizeof(int));
+            }
+            else
+            {
+                // Lista con al menos un elemento
+                arreglo = realloc(arreglo, sizeof(int) * (tamanio + 1));
+            }
+            arreglo[tamanio] = valor;
+            tamanio++;
+        }
+        else if (seleccion == 1)
+        {
+            // Eliminar
+            if (tamanio == 0)
+            {
+                printf("Lista vacia\n");
+            }
+            else
+            {
+                valor = arreglo[0];
+                if (tamanio == 1)
+                {
+                    // Queda un elemento
+                    free(arreglo);
+                    arreglo = NULL;
+                    tamanio = 0;
+                }
+                else
+                {
+                    // Quedan al menos dos elementos
+                    // Movemos los elementos un espacio hacia atras
+                    memcpy(arreglo, arreglo + 1, (tamanio - 1) * sizeof(int));
+
+                    arreglo = realloc(arreglo, sizeof(int) * (tamanio - 1));
+                    tamanio--;
+                }
+                printf("Elemento eliminado: %i\n", valor);
+            }
+        }
+        else if (seleccion == 2)
+        {
+            // Listar
+            printf("[ ");
+            for (int i = 0; i < tamanio; i++)
+            {
+                printf("%i ", arreglo[i]);
+            }
+            printf("]\n");
+        }
+        else
+        {
+            // Salir
+            printf("Adios tonoto 🫪\n");
+        }
+
+        if (seleccion != 3)
+            pausa();
+
+    } while (seleccion != 3);
+
+    return 0;
+}
